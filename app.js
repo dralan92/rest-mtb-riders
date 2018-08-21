6f15f18b3262105f2d758934c9e806d875cd13c8
unknown <codename100.alan@gmail.com>
2018-08-21 03:11:37 -0400

    Added logging module morgan

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const riderRoutes = require('./api/routes/riders');
+
+app.use(morgan('dev'));
 
 app.use('/riders', riderRoutes);
 
