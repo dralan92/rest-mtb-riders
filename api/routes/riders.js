5c974216c8116809feb0db4748093c94ed87c25e
unknown <codename100.alan@gmail.com>
2018-08-21 04:26:40 -0400

    posted a test rider.and working fine

--- api/routes/riders.js
+++ api/routes/riders.js
@@ -8,8 +8,21 @@ router.get('/',(req,res,next)=>{
 });
 //post
 router.post('/',(req,res,next)=>{
+    const rider = {
+        username: req.body.username,
+        password: req.body.password,
+        zip: req.body.zip,
+        address: req.body.address,
+
+        gender: req.body.gender,
+        email: req.body.email,
+        dateOfBirth: req.body.dateOfBirth,
+        phone: req.body.phone
+
+    }
     res.status(201).json({
-        message: "Handling post request to /riders"
+        message: "Handling post request to /riders",
+        createdRider: rider
     });
 });
 
--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const riderRoutes = require('./api/routes/riders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+
+}));
+
+app.use(bodyParser.json());
 
 app.use('/riders', riderRoutes);
 
