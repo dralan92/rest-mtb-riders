3232c1eb1ad2095901e4bfdf3787f9f83f246dd8
unknown <codename100.alan@gmail.com>
2018-08-21 03:15:49 -0400

    Added error handling for invalid url

--- app.js
+++ app.js
@@ -8,5 +8,20 @@ app.use(morgan('dev'));
 
 app.use('/riders', riderRoutes);
 
+app.use((req,res,next)=>{
+    const error = new Error('Not Found');
+    error.status =404;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message,
+            code: error.status
+        }
+    });
+});
 
 module.exports = app;
