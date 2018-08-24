7172ba6108acba988c8ff4535e16cb11dfbddc5f
unknown <codename100.alan@gmail.com>
2018-08-24 04:01:36 -0400

    Testing CORES module to overcome the situation

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
+const cors = require('cors');
+
 
 
 const riderRoutes = require('./api/routes/riders');
@@ -35,6 +37,7 @@ app.use(bodyParser.json());
     }
 });*/
 
+app.use(cors());
 app.use('/riders', riderRoutes);
 
 app.use((req,res,next)=>{
