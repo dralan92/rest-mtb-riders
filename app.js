38ab43ee5c81ec4ae4e1da1a8f745c3cc6a572c6
unknown <codename100.alan@gmail.com>
2018-08-24 02:48:21 -0400

    Added CORS Headers

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    
+    if( req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', '*');
+        return res.status(200).json({});
+    }
+});
+
 app.use('/riders', riderRoutes);
 
 app.use((req,res,next)=>{
